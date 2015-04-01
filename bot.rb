ba8dd949b6fe5500b50da4ea324c4607f158ed68
Nat Welch <nat@natwelch.com>
2015-04-01 19:51:23 +0000

    oh right, ruby.

--- bot.rb
+++ bot.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'bundler'
 Bundler.require(:default, ENV['RACK_ENV'] || :development)
-require 'net/http'
+require 'open-uri'
+
 
 # Zulip API Docs: https://zulip.com/api/endpoints/
 # Forcast.io Docs: https://developer.forecast.io/docs/v2
@@ -22,10 +23,5 @@ def weather
   lat = "40.720780"
   long = "-74.001119"
   forecast_url = "https://api.forecast.io/forecast/#{WEATHER_KEY}/#{lat},#{long}"
-
-  # There are totally simpler ways of writing this, just felt like being verbose.
-  url = URI.parse(forecast_url)
-  req = Net::HTTP::Get.new(url.to_s)
-  res = Net::HTTP.start(url.host, url.port) {|http| http.request(req) }
-  res.body
+  return open(forecast_url).read
 end
